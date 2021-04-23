b3c30359d9349aaa9801517e6b1db7a283e380f3
marques-j-robinson <marques.j.robinson@gmail.com>
2021-04-23 16:50:17 -0700

    refactor - only post to twitter

--- main.py
+++ main.py
@@ -1,56 +1,36 @@
-# Python Lib
-import sys
 import os
+import tweepy
+from dotenv import load_dotenv
+load_dotenv()
 
-# Third Party
-import boto3
-from PIL import Image
 
-# Local
-from lib.twitter import Twitter
-
-# Basic Definitions
-BUCKET = 'mrp-paintings'
 IMG_FILE = 'original.jpg'
-SM_IMG_FILE = 'small.jpg'
-BASE_WIDTH = 300
-DEFAULT_STATUS = '#bobross #landscapepainting #oilpainting'
+DEFAULT_MESSAGE = '#bobross #landscapepainting #oilpainting'
 
-# Third Party Clients
-S3 = boto3.client('s3', region_name='us-east-2')
-TW = Twitter()
-
-
-def image_check():
-    return os.path.exists(IMG_FILE)
+# Twitter auth values
+CONSUMER_KEY = os.environ.get('TW_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TW_CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('TW_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('TW_ACCESS_TOKEN_SECRET')
 
 
-def resize():
-    print('RESIZING_IMAGE...')
-    img = Image.open(IMG_FILE)
-    wpercent = (BASE_WIDTH / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((BASE_WIDTH, hsize), Image.ANTIALIAS)
-    img.save(SM_IMG_FILE)
-
-
-def s3_upload(name):
-    print('S3_IMAGE_UPLOAD...')
-    S3.upload_file(SM_IMG_FILE, BUCKET, f'{name}/{SM_IMG_FILE}')
-    S3.upload_file(IMG_FILE, BUCKET, f'{name}/{IMG_FILE}')
-
-
-def main(name):
-    if image_check() is False:
+def main():
+    if os.path.exists(IMG_FILE) is False:
         print('MISSING_MAIN_IMAGE')
         print('Exiting...')
         return
-    resize()
-    s3_upload(name)
-    status = f'{name} {DEFAULT_STATUS}'
-    TW.tweet(SM_IMG_FILE, status)
+    title = input('Enter Title: ')
+    status = f'{title} {DEFAULT_MESSAGE}'
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    TW = tweepy.API(auth)
+    try:
+        TW.verify_credentials()
+    except:
+        print('TW_AUTH ERROR')
+    print('TW_POSTING_IMAGE...')
+    TW.update_with_media(IMG_FILE, status)
 
 
 if __name__ == '__main__':
-    name = sys.argv[1] # TODO error check when name is not provided
-    main(name)
+    main()
